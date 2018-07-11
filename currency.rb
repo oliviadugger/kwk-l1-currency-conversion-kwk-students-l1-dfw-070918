f60b9a4e9f0747bdde27cf26e7df7a268ad7a548
oliviadugger <oliviacdugger@gmail.com>
2018-07-11 17:25:26 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,20 @@
 # Write your cod here
+
+# challenge 1
+def usd_to_eur
+  eur = usd/1.2
+end
+
+
+
+def eur_to_usd
+  usd = eur*1.2
+end
+
+# challenge 2
+
+def usd_to_jpy
+  jpy = eur*111.91
+end
+
+
